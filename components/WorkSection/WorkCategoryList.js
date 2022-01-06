b0186f34de8f53e05f822cee3c39c9c1e6ba8e0f
Moritz Wright <me@moritzwright.com>
2022-01-06 14:58:39 -0600

    animation worksection

--- components/WorkSection/WorkCategoryList.js
+++ components/WorkSection/WorkCategoryList.js
@@ -1,38 +1,105 @@
+import { useEffect } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 // style
 import style from "../../styles/WorkSection.module.css";
 
-const WorkCategoryList = ({ data }) => {
+const WorkCategoryList = ({ type, image }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.4,
+  });
+
+  const charLines = type.match(/.{1,4}/g);
+
   const animation = {
-    border: (i) => ({}),
-    category: (i) => ({}),
-    image: (i) => ({}),
+    border: {
+      x: "0%",
+      transition: {
+        delay: 0.2,
+        duration: 1,
+        ease: "easeOut",
+      },
+    },
+    image: {
+      y: "0%",
+      transition: {
+        delay: 0.4,
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    },
   };
+
+  const lineAnimation = {
+    lines: (j) => ({
+      y: "0%",
+      transition: {
+        delay: 0.6 + j * 0.2,
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    }),
+  };
+
+  const imageControl = useAnimation();
+  const borderControl = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      imageControl.start("image");
+      borderControl.start("border");
+    }
+  }, [inView, imageControl, borderControl]);
+
   return (
     <>
-      {data.works.map((work, i) => {
-        console.log(work);
-        return (
-          <div key={work.slug}>
-            <div className={style.border}></div>
-            <div className={style.cardContent}>
-              <div className={style.categoryContainer}>
-                <h3 className={style.categoryHealine}>{work.artworkType}</h3>
-              </div>
-              <div className={style.imageContainer}>
-                <Image
-                  src={work.coverImage.url}
-                  alt={work.title}
-                  layout="fill"
-                  objectFit="contain"
-                />
-              </div>
-            </div>
+      <div ref={ref}>
+        <div style={{ overflow: "hidden" }}>
+          <motion.div
+            className={style.border}
+            initial={{ x: "100%" }}
+            variants={animation}
+            animate={borderControl}
+          ></motion.div>
+        </div>
+        <div className={style.cardContent}>
+          <div className={style.categoryContainer}>
+            <h3 className={style.categoryHeadline}>
+              {charLines.map((line, j) => (
+                <div key={j} className={style.overflow}>
+                  {inView && (
+                    <motion.div
+                      custom={j}
+                      initial={{ y: "100%" }}
+                      variants={lineAnimation}
+                      animate="lines"
+                    >
+                      {line}
+                    </motion.div>
+                  )}
+                </div>
+              ))}
+            </h3>
           </div>
-        );
-      })}
+          <div className={style.overflow}>
+            <motion.div
+              className={style.imageContainer}
+              initial={{ y: "100%" }}
+              variants={animation}
+              animate={imageControl}
+            >
+              <Image
+                src={image.url}
+                alt={image.fileName}
+                layout="fill"
+                objectFit="contain"
+              />
+            </motion.div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- components/WorkSection/index.js
+++ components/WorkSection/index.js
@@ -7,29 +7,14 @@ import { useInView } from "react-intersection-observer";
 import style from "../../styles/WorkSection.module.css";
 
 const WorkSection = ({ data }) => {
-  const { ref, inView } = useInView({
-    threshold: 1,
-  });
-
-  //   const animation = {
-  //     image: {
-  //       opacity: 1,
-  //       y: "0%",
-  //       transition: { bounce: 0, ease: "easeOut", duration: 0.5 },
-  //     },
-  //   };
-
-  //   const imgControl = useAnimation();
-
-  //   useEffect(() => {
-  //     if (inView) {
-  //       imgControl.start("image");
-  //     }
-  //   }, [inView, imgControl]);
   return (
-    <section ref={ref} className={style.container}>
+    <section className={style.container}>
       <h2 className={style.sectionHeadline}>Werke</h2>
-      <WorkCategoryList data={data} />
+      {data.works.map((data, i) => (
+        <div key={i}>
+          <WorkCategoryList type={data.artworkType} image={data.coverImage} />
+        </div>
+      ))}
     </section>
   );
 };
